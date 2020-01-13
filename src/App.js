ca838780606dffa8bdd86981008e99912d140ec9
James Bradford <j.bradford@pnnl.gov>
2020-01-12 19:49:50 -0800

    added dev script

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { Button } from '@blueprintjs/core'
 function App() {
   return (
     <div className="App">
+      <Button text="Hello World" />
       <Button text="Hello World" intent="primary" />
+      <Button text="Hello World" minimal />
+      <Button text="Hello World" intent="primary" minimal />
     </div>
   );
 }
