44f02f5da4f982755d75f605a5309358c4812320
James Bradford <j.bradford@pnnl.gov>
2020-01-12 17:25:15 -0800

    adding create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './themes/blueprint.css';
+import { Button } from '@blueprintjs/core'
+
+function App() {
+  return (
+    <div className="App">
+      <Button text="Hello World" />
+    </div>
+  );
+}
+
+export default App;
