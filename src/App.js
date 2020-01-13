e93227fea19dc1dab10c56fa5580b17e5e73b927
James Bradford <j.bradford@pnnl.gov>
2020-01-12 18:03:48 -0800

    adding custom theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import './themes/blueprint.css';
+import './themes/blueprint-custom-theme.css';
 import { Button } from '@blueprintjs/core'
 
 function App() {
   return (
     <div className="App">
-      <Button text="Hello World" />
+      <Button text="Hello World" intent="primary" />
     </div>
   );
 }
