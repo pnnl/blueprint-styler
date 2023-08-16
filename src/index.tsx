0da17c378202b83eeb2a4d256c4779f549aee0ee
James Bradford <j.bradford@pnnl.gov>
2023-08-15 19:15:08 -0700

    update ReactDOM.render() to v18 createRoot().render()

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import { HotkeysProvider } from '@blueprintjs/core';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import BlueprintStylerApp from './components/BlueprintStylerApp';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// After
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
     <React.StrictMode>
         <HotkeysProvider>
             <BrowserRouter>
                 <BlueprintStylerApp />
             </BrowserRouter>
         </HotkeysProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
